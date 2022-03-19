0f9db6cde91e245b80a73b131b3789c4ad4b6750
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2022-03-19 17:31:47 -0300

    booking methods added

--- Selenium/3 - booking_main.py
+++ Selenium/3 - booking_main.py
@@ -2,3 +2,6 @@ from Booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
+    # bot.change_currency()
+    bot.select_destination('São Paulo')
+    bot.select_date(checkin='2022-03-20', checkout = '2022-04-16')
--- Selenium/Booking/booking.py
+++ Selenium/Booking/booking.py
@@ -1,3 +1,4 @@
+from select import select
 from selenium import webdriver
 import os
 import Booking.constants as const
@@ -8,6 +9,8 @@ class Booking(webdriver.Chrome):
         self.teardown = teardown
         os.environ['PATH'] += os.pathsep + driver_path
         super(Booking, self).__init__()
+        self.implicitly_wait(15)
+        self.maximize_window()
     
     def __exit__(self, *args):
         if self.teardown:
@@ -15,4 +18,22 @@ class Booking(webdriver.Chrome):
     
     def land_first_page(self):
         self.get(const.BASE_URL)
+    
+    def change_currency(self, currency = 'BRL'):
+        currency_button = self.find_element_by_css_selector('button[data-modal-aria-label = "Selecione sua moeda"]')
+        currency_button.click()
+        selected_currency_element = self.find_element_by_css_selector(f'a[data-modal-header-async-url-param*="selected_currency={currency}"]')
+        selected_currency_element.click()
         
+    def select_destination(self, destination):
+        search_field = self.find_element_by_id('ss')
+        search_field.clear()
+        search_field.send_keys(destination)
+        first_result = self.find_element_by_css_selector('li[data-i="0"]')
+        first_result.click()
+        
+    def select_date(self, checkin, checkout):
+        checkin_element = self.find_element_by_css_selector(f'td[data-date="{checkin}"')
+        checkin_element.click()
+        checkout_element = self.find_element_by_css_selector(f'td[data-date="{checkout}"')
+        checkout_element.click()
