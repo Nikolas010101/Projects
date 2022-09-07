a71896f186e618882d7bc007cac148fca3f26e5e
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2022-09-07 18:26:28 -0300

    OpenCV folder

---
+++ Deep learning/OpenCV/Basics/reading_videos.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+# 0 refers to 0th camera plugged in
+capture = cv.VideoCapture(0)
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    # Stops when d is pressed cv.waitKey is interval between frames
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
