ed4d0f77b71b4ef5f5a5286a0ad4ce89c8df8e54
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2022-09-10 18:14:54 -0300

    Color spaces

---
+++ Deep learning/OpenCV/Basics/color_spaces.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+img = cv.imread("Projects\Deep learning\OpenCV\Photos\park.jpg")
+cv.imshow("Boston", img)
+
+# BGR to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("Gray", gray)
+
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow("HSV", hsv)
+
+# BGR to LAB
+lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+cv.imshow("LAB", lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow("RGB", rgb)
+
+cv.waitKey(0)
