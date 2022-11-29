d4f60d29556b1f210773c81778a1c811dc7ed299
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2022-11-28 22:49:05 -0300

    Update Resume

--- resume/src/components/Skill.jsx
+++ resume/src/components/Skill.jsx
@@ -1,57 +1,31 @@
 import React from 'react'
 
-export default function Skill({ level, text, tagStyle }) {
+export default function Skill({ level, text }) {
     const size = 24
-    let svgArray;
-    switch (tagStyle) {
-        case 'bar':
-            svgArray = Array.from(Array(5)).map((_, i) => {
-                const color = i < level ? "#92D050" : "#4E6773"
-                const invColor = i < level ? "#4E6773" : "#92D050"
-                return (
-                    <svg key={i} height={size} width={size} style={{ alignSelf: "center" }}>
-                        <rect key={i * 10} width={size} height={size} stroke={invColor} strokeWidth="3" fill={color}></rect>
-                    </svg>
-                )
-            })
-            break;
-        default:
-            svgArray = Array.from(Array(5)).map((_, i) => {
-                const color = i < level ? "#92D050" : "#4E6773"
-                const invColor = i < level ? "#4E6773" : "#92D050"
-                return (
-                    <svg key={i} height={size} width={size} style={{ alignSelf: "center" }}>
-                        <circle key={i * 10} cx={size / 2} cy={size / 2} stroke={invColor} strokeWidth="2" r={size / 3} fill={color}></circle>
-                    </svg>
-                )
-            })
-            break;
-    }
+    const svgArray = Array.from(Array(5)).map((_, i) => {
+        const color = i < level ? "#356258" : "#78909c"
+        return (
+            <svg className={i < level ? "level-active" : ""} key={i} height={size} width={size} style={{ alignSelf: "center" }}>
+                <circle key={i * 10} cx={size / 2} cy={size / 2} stroke="#607d8b" strokeWidth="4" r={size / 3} fill={color}></circle>
+            </svg>
+        )
+    })
     const containerStyle = {
-        display: "grid"
-    }
-    const svgContainerStyle = {
-        gridRow: "1",
-        gridColumn: "1",
-        justifySelf: "start",
-        alignSelf: "center",
-        marginLeft: "10px"
+        display: "grid",
+        gridTemplateRows: "auto auto",
+        placeItems: "center"
     }
     const labelStyle = {
-        fontSize: "1.75em",
-        fontWeight: "bold",
-        fontFamily: "Arial, Helvetica, sans-serif",
+        fontSize: "1.5em",
         color: "white",
-        gridRow: "1",
-        gridColumn: "2",
-        marginRight: "10px"
+        margin: "0px 50px 0px 50px"
     }
     return (
         <div style={containerStyle}>
-            <div style={svgContainerStyle}>
+            <p style={labelStyle}>{text}</p>
+            <div>
                 {svgArray}
             </div>
-            <p style={labelStyle}>{text}</p>
         </div>
     )
 }
